b4a2506aaa47906922ad5e5f447f152bcb8e493d
zMidair <midair4546@gmail.com>
2024-10-26 10:33:18 +0300

    rewrote website

--- home.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".my-skills", {
-    strings: ["Level Showcaser", "Geode Mod Developer", "Average Gamer"],
-    typeSpeed: 100,
-    backSpeed: 80,
-    backDelay: 1000,
-    loop: true
-})
