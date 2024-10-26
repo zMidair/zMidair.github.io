446fe9bd3f4e6fdde482c0af9d82df652868847e
zMidair <midair4546@gmail.com>
2024-10-26 13:36:44 +0300

    some more website stuff

---
+++ home.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typing-atributes", {
+    strings: ["Programming enthusiast", "Geode Mod Developer", "GD Player"],
+    typeSpeed: 120,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
